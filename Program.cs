3ac964c4c31d7a63dde8a6c5b400f8d0fa57a9f7
Alex <sin27san@gmail.com>
2022-12-08 23:07:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_MVC_Core.tutorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
