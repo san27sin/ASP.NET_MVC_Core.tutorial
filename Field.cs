e03bfe5d7d1f75146d0ffadb01a8963d11856b77
san27sin <sin27san@gmail.com>
2022-12-14 21:39:55 +0300

    Задача №2. Задачу необходимо выполнить в рамках многопоточного программирования, потоки необходимо запускать на пулле потоков (ThreadPool).

---
+++ Field.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASP.NET_MVC_Core.tutorial
+{
+    public class Field
+    {
+        public int Cols { get => field.GetLength(0); }
+        public int Rows { get => field.GetLength(1); }
+
+        public AutoResetEvent[] Handles { get => handles; }
+
+        private char[,] field;
+
+        private AutoResetEvent[] handles;
+
+        public Field(int x, int y)
+        {
+            field = new char[x, y];
+
+            handles = new AutoResetEvent[2]
+            {
+                new AutoResetEvent(false),
+                new AutoResetEvent(false)
+            };
+
+            FillField();
+        }
+
+        private void FillField()
+        {
+            for (int y = 0; y < field.GetLength(0); y++)
+            {
+                for (int x = 0; x < field.GetLength(1); x++)
+                {
+                    field[y, x] = '.';
+                }
+            }
+        }
+
+        public void PrintField()
+        {
+            for (int y = 0; y < field.GetLength(0); y++)
+            {
+                for (int x = 0; x < field.GetLength(1); x++)
+                {
+                    Console.Write($"{field[y, x]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool Plant(int x, int y, char seed)
+        {
+            if (x < 0 || x >= field.GetLength(0) || y < 0 || y >= field.GetLength(1))
+                return false;
+            if (seed != 'X' && seed != '0')
+                return false;
+            if (field[x, y] == '.')
+            {
+                field[x, y] = seed;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ASP.NET_MVC_Core.tutorial
 {
+  
     public class Program
     {
+        
         static void Main(string[] args)
         {
+            Console.Write("Укажите размер поля по оси X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Укажите размер поля по оси Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            var field = new Field(x, y); 
+            field.PrintField();
 
+
+            ThreadPool.QueueUserWorkItem(Farmer0, field);
+            ThreadPool.QueueUserWorkItem(FarmerX, field);
+
+            WaitHandle.WaitAll(field.Handles);
+
+            Console.WriteLine();
+            field.PrintField();
+            Console.ReadKey(true);
+        } 
+
+
+        public static void FarmerX(object o)
+        {
+            if (o!=null && o is Field)
+            {
+                var field = (Field)o;
+                for(int y = 0; y < field.Rows;y++)
+                {
+                    for(int x = 0; x < field.Cols;x++)
+                    {
+                        if (field.Plant(x, y, 'X'))
+                            Thread.Sleep(10);
+                        else
+                            continue;
+                    }
+                }
+                field.Handles[0].Set();
+            }
+        }
+
+        public static void Farmer0(object o)
+        {
+            if (o != null && o is Field)
+            {
+                var field = (Field)o;
+                for(int x = field.Cols; x >= 0; x--)
+                {
+                    for(int y = field.Rows; y >= 0; y--)
+                    {
+                        if (field.Plant(x, y, '0'))
+                            Thread.Sleep(10);
+                        else
+                            continue;
+                    }
+                }
+
+                field.Handles[1].Set();
+            }
         }
     }
 }
