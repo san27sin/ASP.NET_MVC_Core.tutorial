268f0dcf3254a01a4f36e6723edf6d69147a028e
Alex <sin27san@gmail.com>
2022-12-17 14:35:48 +0300

    Построить три класса (базовый и 2 потомка), описывающих некоторых работников с почасовой оплатой (один из потомков) и фиксированной оплатой (второй потомок). а) Описать в базовом классе абстрактный метод для расчёта среднемесячной заработной платы. Для «повременщиков» формула для расчета такова: «среднемесячная заработная плата = 20.8 * 8 * почасовая ставка», для работников с фиксированной оплатой «среднемесячная заработная плата = фиксированная месячная оплата». б) Создать на базе абстрактного класса массив сотрудников и заполнить его, вывести список сотрудников на экран. Домашняя работа посвящена теме урока "Порождающие паттерны"

--- Program.cs
+++ Program.cs
@@ -10,7 +10,91 @@ namespace ASP.NET_MVC_Core.tutorial
     {
         static void Main(string[] args)
         {
+            Payment paymentFixed = new FixedPayment(5_000, 20);
+            paymentFixed.employers.Add(new Employer("Alex", "Sinitsyn"));
+            paymentFixed.employers.Add(new Employer("Pasha", "Kruglov"));
+            Console.WriteLine(paymentFixed.ToString());
 
+            Payment paymentPerHour = new PerHourPayment(300);
+            paymentPerHour.employers.Add(new Employer("Kola", "Lisitsyn"));
+            paymentPerHour.employers.Add(new Employer("Masha", "Davasha"));
+            Console.WriteLine(paymentPerHour.ToString());
+
+            Console.ReadKey();
+        }
+
+        public enum Time
+        {
+            hour,
+            month
+        }
+
+
+        public class Employer
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public Employer(string name, string surname)
+            {
+                Name = name;
+                Surname = surname;
+            }
+        }
+
+        public abstract class Payment
+        {
+
+            public List<Employer> employers { get; } = new List<Employer>();
+            public double Wage { get; }
+            public Payment(double wage)
+            {
+                Wage = wage;
+            }
+
+            public abstract double CalculateMonthPayment();
+
+            public override string ToString()
+            {
+                if(employers.Count == 0)
+                    return String.Empty;
+
+                var sb = new StringBuilder();
+                foreach (var employer in employers)
+                    sb.Append($"{employer.Name} {employer.Surname} / получил среднемесячную зарплату {CalculateMonthPayment().ToString()} руб.\n");
+                return sb.ToString();
+            }
+        }
+
+        public class FixedPayment : Payment
+        {
+            public int Days
+            {
+                get{ return _days; }
+                private set
+                {
+                    if (_days < 0)
+                        value = 0;
+                    if (_days > 20)
+                        value = 20;
+                    else
+                        value = _days;
+                }
+            }
+
+            private int _days;
+
+            public FixedPayment(double wage, int days) : base(wage)
+            { _days = days; }
+
+            public override double CalculateMonthPayment() => (double)(this.Wage * _days);
+        }
+
+
+        public class PerHourPayment : Payment
+        {
+            public PerHourPayment(double wage) : base(wage) { }
+            public override double CalculateMonthPayment() => (double)(20.8 * 8  * Wage);
         }
     }
 }
+
