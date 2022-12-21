ca00aa88668c1c41a09949bf96edae6a5d510ae3
Alex <sin27san@gmail.com>
2022-12-21 10:33:45 +0300

    Сделайте эмулятор устройства сканера. Он сканирует (берет данные из какого либо файла), производит фейковые данные о загрузке процессора и памяти. Код должен быть прост, и дальнейшую работу стоит вести только с контрактами данного устройства. Разработать небольшую библиотеку, которая принимает от этого эмулятора байты, сохраняет в различные форматы и мониторит его состояние, записывая в какой-либо лог.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -7,70 +9,107 @@ using System.Threading.Tasks;
 
 namespace ASP.NET_MVC_Core.tutorial
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             IScan scanertype = new PDFScaner();
-            var scaner = new Scaner();
+            var scaner = new Scaner(scanertype, @"C:\Users\Alex\Desktop\Pushkin.txt");
+            scaner.ScanFile();
+            scaner.ConvertToBinary("C:\\Users\\Alex\\source\\repos\\result.dat");
+
+            Console.ReadKey(true);
+            scaner.Dispose();
         }
     }
 
-    interface IScan
+    public interface IScan
     {
-        void Scan();
+        byte[] Scan(string path);
     }
 
     public class PDFScaner : IScan
     {
-        public void Scan()
+        public byte[] Scan(string path)
         {
             //Скан пдф
+            return new byte[] { };
         }
     }
 
     public class DOCXScanner : IScan
     {
-        public void Scan()
+
+        public byte[] Scan(string path)
         {
-            //Скан Docx
+            if (!File.Exists(path))
+                return null;
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            Console.WriteLine($"Имя файла: {fileInfo.Name}");
+            Console.WriteLine($"Время создания: {fileInfo.CreationTime}");
+            Console.WriteLine($"Размер: {fileInfo.Length}");
+            return File.ReadAllBytes(path);
         }
     }
 
     public class QRCodeScanner : IScan
     {
-        public void Scan()
+        public byte[] Scan(string path)
         {
             //Скан QRCodeScanner
+            return new byte[] { };
         }
     }
 
-    public class Scaner
+    public class Scaner : IDisposable
     {
         //Придумать событие которое через каждые 5 секунд показывает нам состояние ЦП и Оперативно памяти
         private IScan _scan;
-        private static TimeSpan period = TimeSpan.FromSeconds(5);
-        private PerformanceCounter cpuCounter;
-        private PerformanceCounter ramCounter;
-
-
+        private static TimeSpan _period = TimeSpan.FromSeconds(5);
+        private PerformanceCounter _cpuCounter;
+        private PerformanceCounter _ramCounter;
+        private string _path;
+        private byte[] _bytes;
 
         public Scaner(IScan scan, string PathToFile)
         {
             _scan = scan;
-            cpuCounter = new PerformanceCounter();
-            cpuCounter.CategoryName = "Processor";
-            cpuCounter.CounterName = "% Processor Time";
-            cpuCounter.InstanceName = "_Total";
-            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            _path = PathToFile;
+            _cpuCounter = new PerformanceCounter();
+            _cpuCounter.CategoryName = "Processor";
+            _cpuCounter.CounterName = "% Processor Time";
+            _cpuCounter.InstanceName = "_Total";
+            _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
         }
 
         public void ScanFile()
         {
-            var timer = new Timer( 
-                x => Console.WriteLine(string.Format("CPU Value: {0}, ram value: {1}", cpu, ram)), 
-                null, period, period);
-            _scan.Scan();
+            var timer = new Timer(
+                x => Console.WriteLine(string.Format($"CPU Value: {_cpuCounter.NextValue()}, ram value: {_ramCounter.NextValue()}"),
+                null, _period, _period));
+            _bytes = _scan.Scan(_path);
+        }
+
+        public bool ConvertToBinary(string pathToSave)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(pathToSave)))
+                return false;
+
+            using (BinaryWriter reader = new BinaryWriter(File.Open(pathToSave, FileMode.OpenOrCreate)))
+            {
+                reader.Write(_bytes);
+            }
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _cpuCounter.Dispose();
+            _ramCounter.Dispose();
         }
     }
+
 }
